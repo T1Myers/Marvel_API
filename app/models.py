fbced3d320a06609fc8c8b94836242cfac3325ce
derekcodingtemple <derekh@codingtemple.come>
2021-09-15 12:03:33 -0500

    added new models and form can now make posts

--- app/models.py
+++ app/models.py
@@ -4,6 +4,13 @@ from datetime import datetime as dt
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text())
+    date_created = db.Column(db.DateTime(), default=dt.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(50))
     last_name = db.Column(db.String(50))
-    date_created = db.Column(db.DateTime(), default=dt.utcnow)
+    email = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
