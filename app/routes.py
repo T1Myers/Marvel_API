afe67cc1801d7282c38d62d9c18d096091127d14
derekcodingtemple <derekh@codingtemple.come>
2021-09-15 15:13:10 -0500

    users can login and logout

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,13 @@
 from flask.helpers import url_for
+import flask_login
 from app import app, db
 from flask import render_template, request, redirect, url_for, flash
-from app.models import Post
+from app.models import Post, User
+from flask_login import login_user, logout_user, current_user
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
+    print(current_user if current_user else None)
     if request.method == 'POST':
         p = Post(
                 body=request.form.get('body'),
@@ -27,3 +30,30 @@ def about():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # look for the user in our database
+        user = User.query.filter_by(email=email).first()
+        # if the email and/or password don't match,
+        if user is None or not user.check_password(password):
+            # show an error messages
+            flash('You typed in either an incorrect email or password', 'danger')
+            # redirect to the login page
+            return redirect(url_for('login'))
+        # otherwise
+        # log the user in
+        login_user(user)
+        flash('You have logged in successfully!', 'info')
+        return redirect(url_for('home'))
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    flash('You have logged out successfully', 'primary')
+    return redirect(url_for('home'))
